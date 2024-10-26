1ecac20470f63c1a3d2d0041ba5cddc0935f2183
Sabuj-Chowdhury <csabuj73@gmail.com>
2024-10-26 15:41:23 +0530

    Blog component with some styles from fake JSON

--- cafe-knowledge/src/Components/Blog/Blog.jsx
+++ cafe-knowledge/src/Components/Blog/Blog.jsx
@@ -1,6 +1,43 @@
 import PropTypes from "prop-types";
 const Blog = ({ blog }) => {
-  return <div></div>;
+  const {
+    id,
+    title,
+    cover,
+    author,
+    author_img,
+    posted_date,
+    reading_time,
+    hashtags,
+  } = blog;
+  return (
+    <div>
+      <img
+        className="w-full rounded-lg mt-2"
+        src={cover}
+        alt={`cover picture of ${title}`}
+      />
+      <div className="flex justify-between items-center mt-3">
+        <div className="flex justify-center">
+          <img className="w-14" src={author_img} alt="" />
+          <div className="ml-4">
+            <p className="text-2xl font-bold">{author}</p>
+            <p>{posted_date}</p>
+          </div>
+        </div>
+        <div>
+          <span>{reading_time} mins read</span>
+        </div>
+      </div>
+
+      <h2 className="text-4xl">{title}</h2>
+      {hashtags.map((hashtag, idx) => (
+        <span key={idx}>
+          <a href="">#{hashtag}</a>
+        </span>
+      ))}
+    </div>
+  );
 };
 
 Blog.propTypes = {
--- cafe-knowledge/src/Components/Blogs/Blogs.jsx
+++ cafe-knowledge/src/Components/Blogs/Blogs.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import Blog from "../Blog/Blog";
 
 const Blogs = () => {
   const [blogs, setBlogs] = useState([]);
@@ -10,6 +11,9 @@ const Blogs = () => {
   return (
     <div className="md:w-2/3">
       <h2 className="text-4xl">Blogs:{blogs.length} </h2>
+      {blogs.map((blog) => (
+        <Blog key={blog.id} blog={blog}></Blog>
+      ))}
     </div>
   );
 };
